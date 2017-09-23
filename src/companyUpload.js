328bd108448b625ca920a1fd3f7c545bb5f95219
imdyangs <d65yang@gmail.com>
2017-09-23 13:42:55 -0400

    added firebase

--- src/companyUpload.js
+++ src/companyUpload.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import BrowseButton from 'react-toolbox/lib/button/BrowseButton';
-
-
+import fire from './fire';
 import './companyUpload.css';
 
 class companyUpload extends Component {
@@ -9,8 +8,34 @@ class companyUpload extends Component {
     super();
     this.state = {
       filePath: '',
+      imageUrl: [],
     };
   }
+  componentWillMount() {
+    /* Create reference to messages in Firebase Database */
+    let imageUrlRef = fire
+      .storage()
+      .ref('messages')
+      .orderByKey()
+      .limitToLast(100);
+
+    const imageUrlArray = firebase.storage();
+    const imageRef = fire.storage().object();
+    imageRef.on('child_added', snapshot => {
+      console.log('new child added')
+    })
+  }
+
+  addMessage(e) {
+    e.preventDefault(); // <- prevent form submit from reloading the page
+    /* Send the message to Firebase */
+    fire
+      .database()
+      .ref('messages')
+      .push(this.inputEl.value);
+    this.inputEl.value = ''; // <- clear the input
+  }
+
   updateUrl = e => {
     console.log(e.target.files[0]);
     const file = e.target.value;
@@ -19,9 +44,7 @@ class companyUpload extends Component {
     });
   };
 
-  submitUrl = e => {
-
-  }
+  submitUrl = e => {};
 
   render() {
     return (
