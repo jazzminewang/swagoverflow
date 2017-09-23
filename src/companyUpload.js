19749037e7edff04085524cb603773438b558cee
imdyangs <d65yang@gmail.com>
2017-09-23 10:59:34 -0400

    finished upload

--- src/companyUpload.js
+++ src/companyUpload.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import { getFilePaths } from './uploadFunctions';
-// Imports the Google Cloud client library
-import './App.css';
+import BrowseButton from 'react-toolbox/lib/button/BrowseButton';
+
+
+import './companyUpload.css';
 
 class companyUpload extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filePath: '',
+    };
+  }
+  updateUrl = e => {
+    console.log(e.target.files[0]);
+    const file = e.target.value;
+    this.setState({
+      file,
+    });
+  };
+
+  submitUrl = e => {
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="upload">
+        <BrowseButton onChange={this.updateUrl} />
       </div>
     );
   }
