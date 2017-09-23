b69fe76e6bac98cd84945a294d96e50a00309dc5
imdyangs <d65yang@gmail.com>
2017-09-23 16:48:23 -0400

    finished image upload

---
+++ webapp_main/src/components/UploadImage.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import BrowseButton from 'react-toolbox/lib/button/BrowseButton';
+import fire from '../fire';
+
+const storage = fire.storage();
+const database = fire.database();
+
+class UploadImage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      file: {},
+      filename: '',
+    };
+  }
+
+  updateUrl = e => {
+    // saving image to storage
+    e.preventDefault();
+    const file = e.target.files[0];
+    const filename = file.name;
+    console.log(file);
+    const storageRef = storage.ref().child('images');
+    const photoRef = storageRef.child(file.name);
+    const uploadTask = photoRef.put(file);
+    uploadTask.on('state_changed', null, null, (snapshot) => {
+      console.log("getting url")
+      // save url to database
+      photoRef
+        .getDownloadURL()
+        .then(url => {
+          console.log("saving url")
+          const newRef = database.ref('imageURLs').push();
+          newRef.set({
+            filename,
+            url
+          });
+        });
+    });
+  };
+
+  update = _.once(this.updateUrl);
+
+  render() {
+    return (
+      <div className="upload">
+        <BrowseButton onChange={this.update} />
+      </div>
+    );
+  }
+}
+
+export default UploadImage;
