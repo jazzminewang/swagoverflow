4bf4a61682bc2fdb9b6c1ff24f734f1820386c13
Jasmine Wang <jasminewang76@gmail.com>
2017-09-23 01:10:06 -0400

    set up firebase

--- poc_webupload2/src/App.js
+++ poc_webupload2/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as admin from "firebase-admin";
+
 
 class App extends Component {
   render() {
